f8d442af4a98ea9ae907e9cfbaaaf0e5c6105942
Tyuiu.MeshcheryakovPV@outlook.com <pavellol202@gmail.com>
2023-10-20 20:05:54 +0300

    Task1

---
+++ Tyuiu.MeshcheryakovPV.Sprint3.Task1.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MeshcheryakovPV.Sprint3.Task1.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MeshcheryakovPV.Sprint3.Task1.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b945e218-69a6-465f-aaa2-0565cf8ce5dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MeshcheryakovPV.Sprint3.Task1.V21/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MeshcheryakovPV.Sprint3.Task1.V21.Lib;
+
+namespace Tyuiu.MeshcheryakovPV.Sprint3.Task1.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Мещеряков П.В. | ИИПБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: АЛГОРИТМЫ ЦИКЛИЧЕСКОЙ СТРУКТУРЫ                                   *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил Мещеряков П.В. | ИИПб-23-2                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 12;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
