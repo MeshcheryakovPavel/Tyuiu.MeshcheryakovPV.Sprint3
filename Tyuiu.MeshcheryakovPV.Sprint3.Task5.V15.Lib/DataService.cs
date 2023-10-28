21b8ed6e919f2a84b60353ce0aedf6d903f24e5f
Tyuiu.MeshcheryakovPV@outlook.com <pavellol202@gmail.com>
2023-10-28 20:45:42 +0300

    Task5

---
+++ Tyuiu.MeshcheryakovPV.Sprint3.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MeshcheryakovPV.Sprint3.Task5.V15.Lib
+{
+    public class DataService : ISprint3Task5V15
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sum = 0;
+            int i, j;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (j = startValue2; j <= stopValue2; j++)
+                {
+                    sum = sum + Math.Sin(x) * (j + Math.Pow(2, 2) / 2);
+                }
+            }
+            return Math.Round(sum, 3);
+        }
+    }
+}
---
+++ Tyuiu.MeshcheryakovPV.Sprint3.Task5.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MeshcheryakovPV.Sprint3.Task5.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MeshcheryakovPV.Sprint3.Task5.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8eccb90-3928-4f9d-8f49-1943a41975b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
